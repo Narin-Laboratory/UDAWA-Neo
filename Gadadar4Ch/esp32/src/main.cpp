7ec9f20f38987b321e937381c48bed37802c7128
iwas108 <64204327+iwas108@users.noreply.github.com>
2024-09-16 15:04:24 +0800

    initial

---
+++ Gadadar4Ch/esp32/src/main.cpp
@@ -0,0 +1,44 @@
+#include "main.h"
+
+Udawa udawa;
+Config config;
+
+void setup() {
+  udawa.begin();
+  
+  if (!udawa.crashState.fSafeMode) {
+    loadConfig();
+  
+    saveConfig();
+  }
+}
+
+unsigned long timer = millis();
+void loop() {
+  udawa.run();
+
+  if (!udawa.crashState.fSafeMode) {
+    if (millis() - timer > 1000) {
+      udawa.logger->debug(PSTR(__func__), PSTR("%d\n"), ESP.getFreeHeap());
+
+      timer = millis();
+    }
+  }
+}
+
+void loadConfig(){
+  JsonDocument doc;
+  bool status = configHelper.load(doc);
+  udawa.logger->debug(PSTR(__func__), PSTR("%d\n"), (int)status);
+  if(status){
+    //if(doc[PSTR("sowingDatetime")] != nullptr){config.sowingDatetime = doc[PSTR("sowingDatetime")].as<String>();} else{config.sowingDatetime = "2024-07-09 08:00:00";}
+
+  }
+}
+
+void saveConfig(){
+  JsonDocument doc;
+  //doc[PSTR("sowingDatetime")] = config.sowingDatetime;
+  bool status = configHelper.save(doc);
+  udawa.logger->debug(PSTR(__func__), PSTR("%d\n"), (int)status);
+}
---
+++ Gadadar4Ch/esp32/src/main.h
@@ -0,0 +1,12 @@
+#include <Arduino.h>
+#include "UdawaLogger.h"
+#include "UdawaSerialLogger.h"
+#include "Udawa.h"
+#include "UdawaConfig.h"
+#include <time.h>
+
+struct Config {
+   
+};
+
+GenericConfig configHelper("/gadadar4ch.json");
